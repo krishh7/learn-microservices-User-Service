85850619feef5fe0f700cf07023237df61cf1265
Krishna Torrem <krishnatorrem@gmail.com>
2023-11-24 20:12:14 +0530

    Implement service discovery with Eureka

--- src/main/java/com/lcwd/user/service/entity/User.java
+++ src/main/java/com/lcwd/user/service/entity/User.java
@@ -1,11 +1,11 @@
 package com.lcwd.user.service.entity;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
